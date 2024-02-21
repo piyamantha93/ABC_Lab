e04adc93c2a8e5413aa49ce6f5f4e82f0818f900
piyamantha03 <piyamantha321@gmail.com>
2024-02-21 20:03:33 +0530

    my frist commit

---
+++ src/main/java/com/abc_lab_system/AbcLabSystemApplication.java
@@ -0,0 +1,13 @@
+package com.abc_lab_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcLabSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbcLabSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abc_lab_system/AbcLabSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc_lab_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcLabSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
